129516a21b0e1b4f5a861d4cb9603d91fcfa98dd
Tigran Andryan <tkandryan@edu.hse.ru>
2022-12-20 22:50:53 +0300

    Bot

---
+++ glossary.py
@@ -0,0 +1,98 @@
+algo_list = "1: Обход в глубину: связность, циклы, двудольность\n\
+2: Обход в глубину: топологическая сортировка, мосты, точки сочленения\n\
+3: Обход в ширину\n\
+4: Алгоритм Дейкстры\n\
+5: Алгоритмы Форда-Беллмана и Флойда\n\
+6: Система непересекающихся множеств, остовные деревья\n\
+7: Двоичные деревья поиска, балансировка\n\
+8: Повторение дерева отрезков\n\
+9: Декартово дерево\n\
+10: Префиксные суммы, разреженные таблицы и другое\n\
+11: Наименьший общий предок, двоичные подъемы\n\
+12: Алгоритмы теории чисел\n\
+13: Комбинаторные объекты\n\
+14: Перебор, метод ветвей и границ\n\
+15: Два указателя, сортировка событий\n\
+16: Корневая оптимизация и другие интересные вещи"
+
+algo_book = {
+    1: 'Обход в глубину: связность, циклы, двудольность',
+    2: 'Обход в глубину: топологическая сортировка, мосты, точки сочленения',
+    3: 'Обход в ширину',
+    4: 'Алгоритм Дейкстры',
+    5: 'Алгоритмы Форда-Беллмана и Флойда',
+    6: 'Система непересекающихся множеств, остовные деревья',
+    7: 'Двоичные деревья поиска, балансировка',
+    8: 'Повторение дерева отрезков',
+    9: 'Декартово дерево',
+    10: 'Префиксные суммы, разреженные таблицы и другое',
+    11: 'Наименьший общий предок, двоичные подъемы',
+    12: 'Алгоритмы теории чисел',
+    13: 'Комбинаторные объекты',
+    14: 'Перебор, метод ветвей и границ',
+    15: 'Два указателя, сортировка событий',
+    16: 'Корневая оптимизация и другие интересные вещи'
+}
+
+algo_video_links = {
+    1: 'https://disk.yandex.ru/i/1b1vuThUDl68Bw',
+    2: 'https://disk.yandex.ru/d/lNbKtaiNmNAGZA',
+    3: 'https://disk.yandex.ru/i/sH3DtRQBp8HOTg',
+    4: 'https://disk.yandex.ru/i/6EuLJbJ-gJTNFQ',
+    5: 'https://disk.yandex.ru/i/EVPuOm7LWKkU3Q',
+    6: 'https://disk.yandex.ru/i/uqLepy_l_Fj9Cw',
+    7: 'https://disk.yandex.ru/i/nUeRZ8LGopw65A',
+    8: 'https://disk.yandex.ru/i/VO6-CTaCgXxcPQ',
+    9: 'https://disk.yandex.ru/i/PsV6J9zVwV2XSA',
+    10: 'https://disk.yandex.ru/i/niZe6Rv6maa_pA',
+    11: 'https://disk.yandex.ru/i/ByAKovvJYJv6rg',
+    12: 'https://disk.yandex.ru/i/PcunFkwfkxNCIQ',
+    13: 'https://disk.yandex.ru/i/Afb4HC38yGoyww',
+    14: 'https://disk.yandex.ru/i/oqTg340JS5g1Hg',
+    15: 'https://disk.yandex.ru/i/E_uyo33luCMdXA',
+    16: 'https://disk.yandex.ru/i/WpUAAHxll7cnhw https://disk.yandex.ru/i/V4A0D28csXy1ag'
+}
+
+algo_tasks = {
+    1: {1: 'https://codeforces.com/problemset/problem/1354/E', 2: 'https://codeforces.com/problemset/problem/1307/D',
+        3: 'https://codeforces.com/problemset/problem/1105/D'},
+    2: {1: 'https://codeforces.com/problemset/problem/1006/E', 2: 'https://codeforces.com/problemset/problem/1000/E',
+        3: 'https://codeforces.com/problemset/problem/999/E'},
+    3: {1: 'https://codeforces.com/problemset/problem/1000/E', 2: 'https://codeforces.com/problemset/problem/999/E',
+        3: 'https://codeforces.com/problemset/problem/1307/D'},
+    4: {1: 'https://codeforces.com/problemset/problem/1370/F2', 2: 'https://codeforces.com/problemset/problem/1335/F',
+        3: 'https://codeforces.com/problemset/problem/1299/D'},
+    5: {1: 'https://codeforces.com/problemset/problem/1253/D', 2: 'https://codeforces.com/problemset/problem/1236/F',
+        3: 'https://codeforces.com/problemset/problem/1220/E'},
+    6: {1: 'https://codeforces.com/problemset/problem/1608/C', 2: 'https://codeforces.com/problemset/problem/1572/D',
+        3: 'https://codeforces.com/problemset/problem/1574/D'},
+    7: {1: 'https://codeforces.com/problemset/problem/1286/B', 2: 'https://codeforces.com/problemset/problem/1272/E',
+        3: 'https://codeforces.com/problemset/problem/1120/D'},
+    8: {1: 'https://codeforces.com/problemset/problem/1003/A', 2: 'https://codeforces.com/problemset/problem/1236/F',
+        3: 'https://codeforces.com/problemset/problem/1220/E'},
+    9: {1: 'https://codeforces.com/problemset/problem/1774/H', 2: 'https://codeforces.com/problemset/problem/1774/B',
+        3: 'https://codeforces.com/problemset/problem/1774/A'},
+    10: {1: 'https://codeforces.com/problemset/problem/1774/A', 2: 'https://codeforces.com/problemset/problem/1253/C',
+         3: 'https://codeforces.com/problemset/problem/1253/B'},
+    11: {1: 'https://codeforces.com/problemset/problem/1773/A', 2: 'https://codeforces.com/problemset/problem/1772/E',
+         3: 'https://codeforces.com/problemset/problem/1774/F2'},
+    12: {1: 'https://codeforces.com/problemset/problem/1773/D', 2: 'https://codeforces.com/problemset/problem/1772/G',
+         3: 'https://codeforces.com/problemset/problem/1771/F'},
+    13: {1: 'https://codeforces.com/problemset/problem/1766/D', 2: 'https://codeforces.com/problemset/problem/1766/D',
+         3: 'https://codeforces.com/problemset/problem/1761/D'},
+    14: {1: 'https://codeforces.com/problemset/problem/1760/C', 2: 'https://codeforces.com/problemset/problem/1760/B',
+         3: 'https://codeforces.com/problemset/problem/1761/C'},
+    15: {1: 'https://codeforces.com/problemset/problem/1761/A', 2: 'https://codeforces.com/problemset/problem/1572/D',
+         3: 'https://codeforces.com/problemset/problem/1574/D'},
+    16: {1: 'https://codeforces.com/problemset/problem/1356/A', 2: 'https://codeforces.com/problemset/problem/1253/C',
+         3: 'https://codeforces.com/problemset/problem/1253/B'}
+}
+
+
+traning_list = {
+    1: {1: 'https://codeforces.com/gym/104036', 2: 'https://codeforces.com/gym/103324', 3: 'https://codeforces.com/gym/100092', 4: 'https://codeforces.com/gym/104035'},
+    2: {1: 'https://codeforces.com/gym/104119', 2: 'https://codeforces.com/gym/104099', 3: 'https://codeforces.com/gym/104069', 4: 'https://codeforces.com/gym/103966'},
+    3: {1: 'https://codeforces.com/gym/104103', 2: 'https://codeforces.com/gym/104092', 3: 'https://codeforces.com/gym/104067', 4: 'https://codeforces.com/gym/103743'},
+    4: {1: 'https://codeforces.com/gym/104118', 2: 'https://codeforces.com/gym/104094', 3: 'https://codeforces.com/gym/104064', 4: 'https://codeforces.com/gym/104022'},
+    5: {1: 'https://codeforces.com/gym/103861', 2: 'https://codeforces.com/gym/103627', 3: 'https://codeforces.com/gym/102978', 4: 'https://codeforces.com/gym/102538'}
+}
---
+++ main.py
@@ -0,0 +1,97 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+from config import BOT_TOKEN
+from glossary import algo_list, algo_book, algo_video_links, algo_tasks, traning_list
+import random
+
+bot = Bot(token=BOT_TOKEN)
+
+dp = Dispatcher(bot)
+
+
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    text = "Привет, дорогой пользователь. Я очень рад, что ты решил изучить алгоритмы! Используй команду /help, чтобы узнать мой функционал!"
+    await bot.send_message(chat_id=message.chat.id, text=text)
+
+
+@dp.message_handler(commands=['help'])
+async def start_message(message):
+    text = "1: /algo - вывести все достпуные для изучения темы.\n" \
+            "2: algo.<b>число</b> - получить ссылку на лекцию по заданной теме, число от 1 до 16\n" \
+            "3: /rand_task  - выдает рандомную задачку с <b>codeforces</b>.\n" \
+            "4: task.<b>число</b> - выдает задачку по заданной теме, число от 1 до 16\n" \
+            "5: training.<b>число</b> - выдает контест. Число указывает сложность контеста, принимает значения от 1 до 5."
+    await bot.send_message(chat_id=message.chat.id, text=text, parse_mode="html")
+
+
+@dp.message_handler(commands=['algo'])
+async def algo_message(message):
+    await bot.send_message(chat_id=message.chat.id, text=algo_list)
+    await bot.send_message(chat_id=message.chat.id, text="Пришли мне сообщение <b>algo.число</b>, и я тебе отправлю лекцию и конспект по выбранной теме!", parse_mode="html")
+
+@dp.message_handler(commands=['rand_task'])
+async def task_message(message):
+    i = random.randint(1, 16)
+    j = random.randint(1, 3)
+    await bot.send_message(chat_id=message.chat.id, text=algo_tasks[i][j])
+
+
+@dp.message_handler()
+async def get_message(message : types.Message):
+    if (len(message.text) > 9 and message.text[0:9] == 'training.'):
+        number = message.text[9:]
+        if (number.isnumeric()):
+            number = int(number)
+            if (number > 5 or number <= 0):
+                await bot.send_message(chat_id=message.chat.id,
+                                       text="Всего 5 уровней сложности(от 1 до 5).")
+            else:
+                i = random.randint(1, 4)
+                await bot.send_message(chat_id=message.chat.id, text="<b>Уровень сложности контеста: " + str(number) + "</b>",
+                                       parse_mode="html")
+                await bot.send_message(chat_id=message.chat.id, text=traning_list[number][i])
+        else:
+            await bot.send_message(chat_id=message.chat.id,
+                                   text=message.text[5:] + " - это не число или чисто меньше 0")
+
+
+
+    elif (len(message.text) > 5 and message.text[0:5] == 'task.'):
+        number = message.text[5:]
+        if (number.isnumeric()):
+            number = int(number)
+            if (number > 16):
+                await bot.send_message(chat_id=message.chat.id,
+                                       text="Я не знаю так много алгоритмов, но скоро точно узнаю, обещаю!")
+                await bot.send_message(chat_id=message.chat.id, text="Число должно быть от 1 до 16")
+            elif (number == 0):
+                await bot.send_message(chat_id=message.chat.id, text="Число должно быть от 1 до 16")
+            else:
+                i = random.randint(1, 3)
+                await bot.send_message(chat_id=message.chat.id, text="<b>Задачка на тему:</b> " + algo_book[number], parse_mode="html")
+                await bot.send_message(chat_id=message.chat.id, text=algo_tasks[number][i])
+        else:
+            await bot.send_message(chat_id=message.chat.id,
+                                   text=message.text[5:] + " - это не число или чисто меньше 0")
+    elif (len(message.text) > 5 and message.text[0:5] == 'algo.'):
+        number = message.text[5:]
+        if (number.isnumeric()):
+            number = int(number)
+            if (number > 16):
+                await bot.send_message(chat_id=message.chat.id, text="Я не знаю так много алгоритмов, но скоро точно узнаю, обещаю!")
+                await bot.send_message(chat_id=message.chat.id, text="Число должно быть от 1 до 16")
+            elif (number == 0):
+                await bot.send_message(chat_id=message.chat.id, text="Число должно быть от 1 до 16")
+            else:
+                await bot.send_message(chat_id=message.chat.id, text="<b>" + algo_book[number] + "</b>", parse_mode="html")
+                await bot.send_message(chat_id=message.chat.id, text=algo_video_links[number])
+
+        else:
+            await bot.send_message(chat_id=message.chat.id, text=message.text[5:] + " - это не число или чисто меньше 0")
+
+    else:
+        await bot.send_message(chat_id=message.chat.id, text="Твоя моя не понимать")
+
+executor.start_polling(dp)
